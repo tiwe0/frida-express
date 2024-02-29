98d4627cac91754a8da826cd54dc0cd52e0f62b9
Ivory <53402365+tiwe0@users.noreply.github.com>
2024-02-29 17:16:05 +0800

    fix packages.json scripts; remove cors restriction;

--- src/be/index.ts
+++ src/be/index.ts
@@ -5,12 +5,14 @@ import { router as RouterSession } from "./session";
 import { router as RouterScript } from "./script";
 import { Intern } from "./interns";
 import express from "express";
+import cors from "cors";
 
 console.log(Intern)
 const app = express()
 var port = 3000
 var host = "localhost"
 
+app.use(cors())
 app.use(express.json())
 app.use("/Intern", RouterIntern)
 app.use("/DeviceManager", RouterDeviceManager)
